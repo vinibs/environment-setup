9384436152c51818e68d0c527480e3c648e1641b
Vinicius Baroni Soares <viniciusbs@ckl.io>
2022-03-11 14:20:58 -0300

    Add gjira dependency fix automation script

--- README.md
+++ README.md
@@ -1,38 +1,45 @@
 # Environment Setup scripts
+
 Shortcuts to setup different development environments.
 
 To make the transition from one machine to another or the setup after formatting, this repository have shell scripts for many different environments that may be needed in a developer day-to-day.
 
 ## Limitations
+
 Currently, this repository only has scripts for setting up the environments in a **macOS** system. All scripts were tested on Intel machines and the ones for Node, Python and Gjira were currently also tested on M1 machines.
 
 ## Requirements
+
 - [Homebrew](https://brew.sh/)
 - macOS 10.15 (Catalina) or newer - or the default terminal set to use [zsh](https://www.zsh.org/)
 
 ## Running
+
 To run any setup the steps are basically the same.
 
 - Clone this repository
 - In a terminal, go to this repository's `/mac` folder
-    ```sh
-    $ cd [repository_path]/mac
-    ```
+
+  ```sh
+  $ cd [repository_path]/mac
+  ```
 
 - Give the desired setup script the permission to execute
-    ```sh
-    $ chmod +x ./node-env.sh
-    ```
+
+  ```sh
+  $ chmod +x ./node-env.sh
+  ```
 
 - Run the shell script
-    ```sh
-    $ ./node-env.sh
-    ```
+  ```sh
+  $ ./node-env.sh
+  ```
 
 > In the examples the `node-env.sh` was run. You can replace the filename with any of the available files as you need, when running.
 >
 > So, for both the steps of give the permission and actually run the script, you could just replace the `node-env.sh` part with one of the currently available options:
 >
+> - `gjira/gjira-dependency-fix.sh`
 > - `gjira-setup.sh`
 > - `mysql-env.sh`
 > - `node-env.sh`
---
+++ mac/gjira/gjira-dependency-fix.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+if ! brew ls --versions pre-commit > /dev/null; then
+    echo "\033[31mError:\033[0m Pre-commit is not installed. Exiting..."
+    exit 1
+fi
+
+if ! (brew ls --versions pyenv > /dev/null && brew ls --versions pyenv-virtualenv > /dev/null); then
+    echo "Pyenv and virtualenv are not installed. Installing...\n"
+    brew install pyenv pyenv-virtualenv &> /dev/null
+
+    echo "\033[33mPlease restart your terminal application and run this script again to ensure the fix will be able to proceed.\033[0m"
+    exit 0
+fi
+
+
+PRE_COMMIT_BASE_DIR=~/.cache/pre-commit/
+
+PRE_COMMIT_PYTHON_ENV_DIR=./py_env-python/bin
+PRE_COMMIT_PYTHON_ENV_DIR_ALT=./py_env-python3/bin
+
+if ! [ -d "$PRE_COMMIT_BASE_DIR" ]; then
+    echo "\033[31mError:\033[0m Pre-commit directory does not exist. Exiting..."
+    exit 1
+fi
+
+cd $PRE_COMMIT_BASE_DIR
+
+REPO_DIRS=( $(file */ | grep ^repo.*/ | awk -F/ '{print $1}') )
+REPO_DIRS_COUNT="${#REPO_DIRS[@]}"
+
+if [ $REPO_DIRS_COUNT == 0 ]; then
+    echo "\033[31mError:\033[0m No Gjira repo directory found. Exiting..."
+    exit 1
+fi
+
+NOT_FOUND_PYTHON_ENV_DIRS=0
+TARGET_MARKUPSAFE_VERSION=2.0.1
+
+for dir in "${REPO_DIRS[@]}"; do
+    cd $dir
+
+    if ! (test -d "$PRE_COMMIT_PYTHON_ENV_DIR" || test -d "$PRE_COMMIT_PYTHON_ENV_DIR_ALT"); then
+        ((NOT_FOUND_PYTHON_ENV_DIRS++))
+        continue
+    fi
+
+    if [ -d "$PRE_COMMIT_PYTHON_ENV_DIR_ALT" ]; then
+        PRE_COMMIT_PYTHON_ENV_DIR=$PRE_COMMIT_PYTHON_ENV_DIR_ALT
+    fi
+
+    cd $PRE_COMMIT_PYTHON_ENV_DIR
+    chmod +x activate
+
+    source ./activate
+
+    CURRENT_MARKUPSAFE_VERSION=`pip show markupsafe | sed -E -n 's/Version: (.+)/\1/p'`
+    if [ $TARGET_MARKUPSAFE_VERSION != $CURRENT_MARKUPSAFE_VERSION ]; then
+        pip install markupsafe==$TARGET_MARKUPSAFE_VERSION
+    fi
+    
+    source deactivate &> /dev/null
+    cd ..
+done
+
+
+if [ "$NOT_FOUND_PYTHON_ENV_DIRS" -gt "0" ]; then
+    if [ $NOT_FOUND_PYTHON_ENV_DIRS == $REPO_DIRS_COUNT ]; then
+        echo "\033[33mNo Gjira repo directory was fixed.\nIts directories were not found.\033[0m"
+    else
+        echo "\033[33mNot all Gjira repo directories could be fixed.\nThere was a total of $NOT_FOUND_PYTHON_ENV_DIRS directories that were not found.\033[0m"
+    fi
+
+    echo "\n\n\033[33mDid you try to make a commit in a project to activate it already?\033[0m"
+    exit 1
+fi
+
+
+echo "\n\033[32mAll done.\033[0m\n"
+echo "\033[33mNow you should be able to successfully use Gjira when making your commits.\033[0m\n"
